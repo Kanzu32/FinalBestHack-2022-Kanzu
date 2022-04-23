53063aa6a4de355edec15474303faca4ba2f4197
sanya12366 <ovsyannikovxxx@yandex.ru>
2022-04-23 12:38:00 +0300

    Шаблон проекта.

---
+++ src/main/java/com/Coders_seem_to_be/FinalBestHack2/FinalBestHack2022Application.java
@@ -0,0 +1,13 @@
+package com.Coders_seem_to_be.FinalBestHack2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBestHack2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBestHack2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Coders_seem_to_be/FinalBestHack2/FinalBestHack2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Coders_seem_to_be.FinalBestHack2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBestHack2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
